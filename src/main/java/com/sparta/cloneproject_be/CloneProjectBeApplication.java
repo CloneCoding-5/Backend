ad19e3a560222210756f86c5e1a3552f5c5970e9
TaeKyeong <murphytklee@gmail.com>
2023-05-12 19:49:27 +0900

    Add: 초기셋팅, Spring Security 추가

---
+++ src/main/java/com/sparta/cloneproject_be/CloneProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneproject_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneProjectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/cloneproject_be/CloneProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneproject_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
