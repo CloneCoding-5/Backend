f6834609daddd9ec5a93daa9cd498d58c5a7f082
TaeKyeong <murphytklee@gmail.com>
2023-05-13 02:58:01 +0900

    Feat : Spring Security + 로그인 + 회원가입
    1. 회원가입 Valid 프론트분들과 상의 후 업데이트 되어야합니다.
    2. 전역 예외 처리로 수정되어야 합니다.
    3. Spring Security, JWT 경우 간소화 해봤습니다.
    4. 토큰에 따로 role 넣지 않았습니다. 테스트 필요합니다.
    5. (@AuthenticationPrincipal User user)로 바로 User 객체 받도록 수정해봤습니다 테스트 필요합니다.

---
+++ src/main/java/com/sparta/cloneproject_be/entity/User.java
@@ -0,0 +1,37 @@
+package com.sparta.cloneproject_be.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
--- src/main/java/com/sparta/cloneproject_be/jwt/JwtAuthFilter.java
+++ src/main/java/com/sparta/cloneproject_be/jwt/JwtAuthFilter.java
@@ -1,59 +1,57 @@
-//package com.sparta.cloneproject_be.jwt;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContext;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//public class JwtAuthFilter extends OncePerRequestFilter {
-//
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        String token = jwtUtil.resolveToken(request);
-//
-//        if(token != null) {
-//            if(!jwtUtil.validateToken(token)){
-//                jwtExceptionHandler(response, "Token Error", HttpStatus.UNAUTHORIZED.value());
-//                return;
-//            }
+package com.sparta.cloneproject_be.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtUtil.resolveToken(request);
+
+        if(token != null) {
+            if(!jwtUtil.validateToken(token)){
+                jwtExceptionHandler(response, "Token Error", HttpStatus.UNAUTHORIZED.value());
+                return;
+            }
 //            Claims info = jwtUtil.getUserInfoFromToken(token);
-//            setAuthentication(info.getSubject());
-//        }
-//        filterChain.doFilter(request,response);
-//    }
-//
-//    public void setAuthentication(String username) {
+//            setAuthentication();
+        }
+        filterChain.doFilter(request,response);
+    }
+
+//    public void setAuthentication(User user) {
 //        SecurityContext context = SecurityContextHolder.createEmptyContext();
-//        Authentication authentication = jwtUtil.createAuthentication(username);
+//        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 //        context.setAuthentication(authentication);
 //
 //        SecurityContextHolder.setContext(context);
 //    }
-//
-//    public void jwtExceptionHandler(HttpServletResponse response, String msg, int statusCode) {
-//        response.setStatus(statusCode);
-//        response.setContentType("application/json");
-//        try {
-//            String json = new ObjectMapper().writeValueAsString(ResponseMsgDto.setFail(statusCode, msg));
-//            response.getWriter().write(json);
-//        } catch (Exception e) {
-//            log.error(e.getMessage());
-//        }
-//    }
-//
-//}
+
+    public void jwtExceptionHandler(HttpServletResponse response, String msg, int statusCode) {
+        response.setStatus(statusCode);
+        response.setContentType("application/json");
+        try {
+            String json = new ObjectMapper().writeValueAsString(ResponseEntity.status(statusCode).body(msg));
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+}
