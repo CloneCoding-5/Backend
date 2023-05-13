0c3b92b3d883947f7973d8e58cfd6e71b7854888
TaeKyeong <murphytklee@gmail.com>
2023-05-13 23:51:11 +0900

    Fix : build 안되는거 수정

--- src/main/java/com/sparta/cloneproject_be/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/cloneproject_be/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.sparta.cloneproject_be.exception;
 
 import com.sparta.cloneproject_be.dto.MessageDto;
-import com.sparta.cloneproject_be.dto.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
