d9baaae9b9a8037e74b45cae88a391de03467316
TaeKyeong <murphytklee@gmail.com>
2023-05-15 19:07:01 +0900

    Feat : WishList 조회 삭제 기능 추가

--- src/main/java/com/sparta/cloneproject_be/controller/WishListController.java
+++ src/main/java/com/sparta/cloneproject_be/controller/WishListController.java
@@ -2,7 +2,9 @@ package com.sparta.cloneproject_be.controller;
 
 import com.sparta.cloneproject_be.dto.MessageDto;
 import com.sparta.cloneproject_be.dto.WishListRequestDto;
+import com.sparta.cloneproject_be.dto.WishListResponseDto;
 import com.sparta.cloneproject_be.entity.User;
+import com.sparta.cloneproject_be.entity.WishList;
 import com.sparta.cloneproject_be.service.WishListService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,10 +13,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Tag(name = "wishListController", description = "위시리스트 API")
 @RestController
@@ -25,10 +27,25 @@ public class WishListController {
     private final WishListService wishListService;
 
     // 위시리스트 추가
-    @Operation(summary = "위시리스트 API", description = "위시리스트 등록")
+    @Operation(summary = "위시리스트 등록 API", description = "위시리스트 등록")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "위시리스트에 추가됐습니다.")})
     @PostMapping("/{roomId}")
-    public ResponseEntity<MessageDto> createWishList(@PathVariable long roomId, WishListRequestDto requestDto, @AuthenticationPrincipal User user){
+    public ResponseEntity<MessageDto> createWishList(@PathVariable long roomId, @RequestBody WishListRequestDto requestDto, @AuthenticationPrincipal User user){
         return wishListService.createWishList(roomId, requestDto, user);
+    }
+    // 위시리스트 조회
+    @Operation(summary = "위시리스트 조회 API", description = "위시리스트 조회")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "조회 성공")})
+    @GetMapping
+    public ResponseEntity<Map<String, List<WishListResponseDto>>> getWishList(@AuthenticationPrincipal User user){
+        return wishListService.getWishList(user);
+    }
+
+    // 위시리스트 삭제
+    @Operation(summary = "위시리스트 삭제 API", description = "위시리스트 삭제")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "위시리스트에서 삭제됐습니다.")})
+    @DeleteMapping("/{wishListId}")
+    public ResponseEntity<MessageDto> deleteWishList(@PathVariable long wishListId, @AuthenticationPrincipal User user){
+        return wishListService.deleteWishList(wishListId, user);
     }
 }
--- src/main/java/com/sparta/cloneproject_be/repository/WishListRepository.java
+++ src/main/java/com/sparta/cloneproject_be/repository/WishListRepository.java
@@ -1,8 +1,15 @@
 package com.sparta.cloneproject_be.repository;
 
+import com.sparta.cloneproject_be.entity.User;
 import com.sparta.cloneproject_be.entity.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 
+    List<WishList> findAllByUser(User user);
+
+    // 삭제
+    void deleteByIdAndUser(Long wishListId, User user);
 }
