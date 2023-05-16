86463e193eb443ca880d90639dc65a5257220e91
JEON <jwodn123@naver.com>
2023-05-16 21:45:54 +0900

    feat : 스크립트 형태로 들어오는 값에 대해 XSS filter 적용

---
+++ src/main/java/com/sparta/cloneproject_be/config/XssConfig.java
@@ -0,0 +1,39 @@
+package com.sparta.cloneproject_be.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class XssConfig implements WebMvcConfigurer  {
+
+    private final ObjectMapper objectMapper;
+
+    // Lucy Xss filter 적용(form-data)
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+        FilterRegistrationBean<XssEscapeServletFilter> xssRegistrationBean = new FilterRegistrationBean<>();
+        xssRegistrationBean.setFilter(new XssEscapeServletFilter());
+        xssRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        xssRegistrationBean.addUrlPatterns("/*");
+        return xssRegistrationBean;
+    }
+
+    // requestBody xss 필터 적용(json/api)
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
+    }
+}
